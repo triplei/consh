4f15b11560fd18dd99a1400470ddb79db0f94b3c
Dan Klassen <dan@tripleiwebsolutions.com>
2014-10-14 22:38:14 -0700

    drop in some code from previous consh version. not functional

--- Core/Commands/Db/Pull.php
+++ Core/Commands/Db/Pull.php
@@ -7,6 +7,7 @@ namespace Consh\Core\Commands\Db;
 
 
 use Consh\Core\Command;
+use Consh\Core\SSHConnection;
 use League\CLImate\CLImate;
 
 class Pull extends Command{
@@ -14,6 +15,15 @@ class Pull extends Command{
     {
         $cli = new CLImate();
         $cli->output("Running Db:Pull");
+        $file_name = 'db_' . time() . '.sql';
+        $remote_file = REMOTE_HOME_PATH.$file_name;
+        $local_file = C5_DIR."{$file_name}";
+
+        $ssh = SSHConnection::getInstance();
+        $ssh->exec('mysqldump -h ' . REMOTE_DB_HOST . ' -u ' . REMOTE_DB_USER . ' -p'.addslashes(REMOTE_DB_PASS) . ' ' . REMOTE_DB_NAME. " > " . $remote_file);
+
+        $ssh->scp($remote_file, $local_file);
+        $ssh->rmRemoteFile($remote_file);
     }
 
     public function help()
